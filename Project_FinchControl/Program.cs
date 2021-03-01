5fa48b188792c485238ea138a4c23d07394add04
tanner Mclean <mcleanta@yahoo.com>
2021-03-01 14:37:09 -0500

    temp

--- Project_FinchControl/Program.cs
+++ Project_FinchControl/Program.cs
@@ -88,11 +88,11 @@ namespace Project_FinchControl
                         break;
 
                     case "c":
-
+                        dataRecorderMenu(doofus);
                         break;
 
                     case "d":
-
+                        
                         break;
 
                     case "e":
@@ -143,6 +143,7 @@ namespace Project_FinchControl
                 Console.WriteLine("\tb) Movement Talent basic");
                 Console.WriteLine("\tc) Play a song");
                 Console.WriteLine("\td) Color calculator");
+                Console.WriteLine("\te) Alarm System");
                 Console.WriteLine("\tq) Main Menu");
                 Console.Write("\t\tEnter Choice:");
                 menuChoice = Console.ReadLine().ToLower();
@@ -167,6 +168,11 @@ namespace Project_FinchControl
                     case "d":
                         colorCalc(doofus);
                         break;
+
+                    case "e":
+                        alarmSystem(doofus);
+                        break;
+
 
                     case "q":
                         quitTalentShowMenu = true;
@@ -772,6 +778,559 @@ namespace Project_FinchControl
 
             return robotConnected;
         }
+
+        #region DATA RECORDER
+
+        //************************
+        //
+        // DATA RECORDER
+        //
+        //*************************
+        static void dataRecorderMenu(Finch doofus)
+        {
+            Console.CursorVisible = true;
+
+            bool quitMenu = false;
+            string menuChoice;
+
+            int numberOfDataPoints = 0;
+            double dataPointFreq = 0;
+            double[] temperatures = null;
+
+            do
+            {
+                DisplayScreenHeader("Data Recorder Menu");
+
+                //
+                // get user menu choice
+                //
+                Console.WriteLine("\ta) Number of Data Points");
+                Console.WriteLine("\tb) Frequency of Data Points");
+                Console.WriteLine("\tc) Get Data");
+                Console.WriteLine("\td) Show Data");
+                Console.WriteLine("\tq) Main Menu");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                //
+                // process user menu choice
+                //
+                switch (menuChoice)
+                {
+                    case "a":
+                        numberOfDataPoints = DataRecorderDisplayNumOfDataPoints();
+                        break;
+
+                    case "b":
+                        dataPointFreq = DataRecorderDiplayGetDataPointFreq();
+                        break;
+
+                    case "c":
+                        temperatures = dataRecorderDisplayGetData(numberOfDataPoints, dataPointFreq, doofus);
+                        break;
+
+                    case "d":
+                        DataRecorderDisplayData(temperatures, numberOfDataPoints);
+                        break;
+
+                    case "q":
+                        quitMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        continueScreen();
+                        break;
+                }
+
+            } while (!quitMenu);
+        }
+        #region TEMPS
+
+       
+        //
+        //Display Temp Table
+        //
+        static void displayTempTable(double[] temperatures)
+        {
+            //
+            //display table of temperatures
+            //
+            Console.WriteLine(
+                "Reading #".PadLeft(10) +
+                "Temperature".PadLeft(15)
+                );
+            Console.WriteLine(
+                "---------".PadLeft(10) +
+                "-----------".PadLeft(15)
+                );
+
+            for (int index = 0; index < temperatures.Length; index++)
+            {
+                Console.WriteLine(
+                      (index + 1).ToString().PadLeft(20) +
+                      (temperatures[index]).ToString("n1").PadLeft(15)
+               );
+            }
+
+        }
+
+        //
+        //Convert To Farenheit
+        //
+        static void convertToF( double[] temperatures, int numberOfDataPoints)
+        {
+            double[] fTemps = new double[numberOfDataPoints];
+            Console.WriteLine();
+            Console.WriteLine("\t Now it will show the Temps in Farenheit");
+            Console.WriteLine();
+
+            for (int index = 0; index < temperatures.Length; index++)
+            {
+                
+                Console.WriteLine(
+                      (index + 1).ToString().PadLeft(20) +
+                      ((((temperatures[index])*9)/5)+32).ToString("n1").PadLeft(15)
+               );
+
+
+
+            }
+
+
+        }
+
+        //
+        //Display Data Table
+        //
+        static void DataRecorderDisplayData(double[] temperatures, int numberOfDataPoints)
+        {
+
+
+            DisplayScreenHeader("Temperatures");
+
+            //table of temps
+            displayTempTable(temperatures);
+
+
+            continueScreen();
+
+            convertToF(temperatures, numberOfDataPoints);
+
+            continueScreen();
+        }
+
+        //
+        // Get Temperature Data from robot
+        //
+        static double[] dataRecorderDisplayGetData(int numberOfDataPoints, double dataPointFreq, Finch doofus)
+        {
+            double[] temperatures = new double[numberOfDataPoints];
+            int dataPointFreqMs;
+
+
+            //
+            //Convert data point freq from seconds to milliseconds
+            //
+            dataPointFreqMs = (int)(dataPointFreq * 1000);
+
+
+            DisplayScreenHeader("Temperatures");
+
+            //
+            //Echo Values
+            //
+            Console.WriteLine($"\tThe Finch robot will now record {numberOfDataPoints} temperatures, {dataPointFreq} seconds apart.");
+            Console.WriteLine("\t Press any button to begin");
+            Console.ReadKey();
+
+
+            for (int index = 0; index < numberOfDataPoints; index++)
+            {
+                temperatures[index] = doofus.getTemperature();
+
+                //
+                //echo new temp
+                //
+                Console.WriteLine($"\t Temperature: {index + 1}: {temperatures[index]:n1} ");
+                doofus.wait(dataPointFreqMs);
+
+                
+
+
+
+            }
+            continueScreen();
+
+            //
+            //display table of temperatures
+            //
+            displayTempTable(temperatures);
+
+
+
+
+            DisplayMenuPrompt("Data Recorder");
+
+            return temperatures;
+        }
+
+        //
+        //Get Data Point Frequency From user
+        //
+        static double DataRecorderDiplayGetDataPointFreq()
+        {
+            string dataPointFreqP;
+            double dataPointFreq;
+            bool validResponse;
+
+            DisplayScreenHeader("Frequency of data points");
+
+            //
+            // TO DO VALIDATE THE ENTRY
+            //
+            Console.WriteLine("\tFrequency Of Data Points:");
+            dataPointFreq = double.Parse(Console.ReadLine());
+
+            do
+            {
+                validResponse = true;
+
+                //
+                //insert temps
+                //
+
+                dataPointFreqP = Console.ReadLine();
+                Console.WriteLine();
+
+                if (!double.TryParse(dataPointFreqP, out dataPointFreq))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"\tPlease enter a number using digits. (10, 35.6, etc...)");
+                    validResponse = false;
+                }
+                else
+                {
+                    Console.WriteLine($"\tYou entered: ${dataPointFreqP}");
+                    Console.WriteLine();
+                    Console.WriteLine("\tPress any button to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (!validResponse);
+
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine($"\tYou Chose {dataPointFreq} as the number of data points.");
+
+            DisplayMenuPrompt("Data Recorder");
+
+            return dataPointFreq;
+        }
+
+        //
+        //Number of Data Points
+        //
+        static int DataRecorderDisplayNumOfDataPoints()
+        {
+            string numberOfDataPoints;
+            int numberOfDataPointsP;
+            bool validResponse;
+
+            DisplayScreenHeader("number of data points");
+
+            //
+            // TO DO VALIDATE THE ENTRY
+            //
+            
+
+            do
+            {
+                validResponse = true;
+
+                //
+                //insert costs
+                //
+                
+                numberOfDataPoints = Console.ReadLine();
+                Console.WriteLine();
+
+                if (!int.TryParse(numberOfDataPoints, out numberOfDataPointsP))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"\tPlease enter a number using Integers. (10, 35, etc...)");
+                    validResponse = false;
+                }
+                else
+                {
+                    Console.WriteLine($"\tYou entered: ${numberOfDataPointsP}");
+                    Console.WriteLine();
+                    Console.WriteLine("\tPress any button to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (!validResponse);
+
+
+
+
+
+            Console.WriteLine();
+            Console.WriteLine($"\tYou Chose {numberOfDataPoints} as the number of data points.");
+
+            DisplayMenuPrompt("Data Recorder");
+
+            return numberOfDataPointsP;
+        }
+
+        #endregion
+
+        #region Alarm System
+
+        //
+        //alarm system menu
+        //
+        static void alarmSystem(Finch doofus)
+        {
+            Console.CursorVisible = true;
+
+            bool quitMenu = false;
+            string menuChoice;
+
+            string sensorsToMonitor = "";
+            string rangeType = "";
+            int minMaxThresholdVal =0;
+            int timeToMonitor =0;
+            
+
+            do
+            {
+                DisplayScreenHeader("Data Recorder Menu");
+
+                //
+                // get user menu choice
+                //
+                Console.WriteLine("\ta) Set Sensors To Monitor");
+                Console.WriteLine("\tb) Set range type");
+                Console.WriteLine("\tc) Set min/max values");
+                Console.WriteLine("\td) set time to monitor");
+                Console.WriteLine("\te) set alarm");
+                Console.WriteLine("\tq) quit");
+                Console.Write("\t\tEnter Choice:");
+                menuChoice = Console.ReadLine().ToLower();
+
+                //
+                // process user menu choice
+                //
+                switch (menuChoice)
+                {
+                    case "a":
+                        sensorsToMonitor = alarmSysDisplaySetSensors();
+                        break;
+
+                    case "b":
+                        rangeType = AlarmSysDisplayRangeType();
+                        break;
+
+                    case "c":
+                        minMaxThresholdVal = AlarmSysDisplayThresholdVal(sensorsToMonitor, doofus);
+                        break;
+
+                    case "d":
+                        timeToMonitor = AlarmSysDisplayTimeToMonitor();
+                        break;
+
+                    case "e":
+                        AlarmSysDiplaySetAlarm(doofus, sensorsToMonitor, rangeType, minMaxThresholdVal);
+                        break;
+
+                    case "q":
+                        quitMenu = true;
+                        break;
+
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("\tPlease enter a letter for the menu choice.");
+                        continueScreen();
+                        break;
+                }
+
+            } while (!quitMenu);
+        }
+
+        //
+        //Set The alarm
+        //
+        static void AlarmSysDiplaySetAlarm(Finch doofus, string sensorsToMonitor, string rangeType, int minMaxThresholdVal)
+        {
+            DisplayScreenHeader("Set Alarm");
+            //
+            //echo values
+            //
+            Console.WriteLine("\tStart");
+
+            //
+            //Prompt Usre to start
+            //
+            continueScreen();
+
+            switch (sensorsToMonitor)
+            {
+                case "left":
+                   
+                    break;
+
+                case "right":
+                   
+                    break;
+
+                case "both":
+                   
+                    break;
+
+                default:
+                    Console.WriteLine("\tERROR: Unknown Sensor Reference");
+                    break;
+            }
+
+
+
+            DisplayMenuPrompt("Alarm System");
+        }
+
+        //
+        //Get Time t monitor
+        //
+        static int AlarmSysDisplayTimeToMonitor()
+        {
+            int timeToMonitor = 0;
+            DisplayScreenHeader("Time To Monitor");
+            //
+            //TO DO USE LOOP TO VALIDATE
+            //
+            Console.Write("\tEnter Time To Monitor:");
+            timeToMonitor = int.Parse(Console.ReadLine());
+
+
+            DisplayMenuPrompt("Alarm System");
+            return timeToMonitor;
+        }
+
+        //
+        //Get Threshold
+        //
+        static int AlarmSysDisplayThresholdVal(string sensorsToMonitor, Finch doofus)
+        {
+            int thresholdVal = 0;
+
+            int currentLeftSensVal = doofus.getLeftLightSensor();
+
+            int currentRightSensVal = doofus.getRightLightSensor();
+
+            DisplayScreenHeader("Threshold Value");
+
+            //
+            //display ambient values
+            //
+            switch (sensorsToMonitor)
+            {
+                case "left":
+                    Console.WriteLine($"Current {sensorsToMonitor} sensor value: {currentLeftSensVal}");
+                    break;
+
+                case "right":
+                    Console.WriteLine($"Current {sensorsToMonitor} sensor value: {currentRightSensVal}");
+                    break;
+
+                case "both":
+                    Console.WriteLine($"Current {sensorsToMonitor} sensor value: {currentLeftSensVal}");
+                    Console.WriteLine($"Current {sensorsToMonitor} sensor value: {currentRightSensVal}");
+                    break;
+
+                default:
+                    Console.WriteLine("\tERROR: Unknown Sensor Reference");
+                    break;
+            }
+
+            //
+            //Get Threshold from user
+            //
+            //
+            // TO DO USE A LOOP TO VALIDATE
+            //
+            Console.Write("\tEnter Threshold Value: ");
+            thresholdVal = int.Parse(Console.ReadLine());
+
+
+            DisplayMenuPrompt("Alarm System");
+
+            return thresholdVal;
+        }
+
+        //
+        //Get Range Type
+        //
+        static string AlarmSysDisplayRangeType()
+        {
+            string rangeType = "";
+
+            DisplayScreenHeader("range type");
+
+            Console.Write("\tEnter Range Type [Minimum, Maximum]:");
+            rangeType = Console.ReadLine();
+            //
+            // TO DO VALIDATE
+            //
+
+
+            DisplayMenuPrompt("Alarm System");
+            return rangeType;
+        }
+
+        //
+        //Sensors to monitor
+        //
+        static string alarmSysDisplaySetSensors()
+        {
+            string sensorsToMonitor = "";
+
+            DisplayScreenHeader("sensors to monitor");
+
+            Console.Write("\tEnter Sensors to Monitor: [Left, Right, Both]");
+
+            //
+            // TO DO VALIDATE THIS
+            //
+            sensorsToMonitor = Console.ReadLine();
+
+            DisplayMenuPrompt("Alarm System");
+
+            return sensorsToMonitor;
+        }
+
+
+
+
+
+
+
+
+
+        #endregion
+
+
+
+
+
+
+        #endregion
+
 
         #endregion
 
